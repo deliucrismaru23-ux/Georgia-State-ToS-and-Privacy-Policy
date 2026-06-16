f5868b96cc165b3a432ef64b09d0cc4e09c9700b
deliucrismaru23-ux <deliucrismaru23@gmail.com>
2026-06-16 11:42:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -167,30 +167,25 @@ version: '10'
 }).setToken(process.env.TOKEN);
 
 async function registerCommands() {
+  try {
+    console.log('Registering commands...');
 
-try {
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      {
+        body: commands
+      }
+    );
 
-console.log('Registering commands...');
-
-await rest.put(
-Routes.applicationGuildCommands(
-  process.env.CLIENT_ID,
-  process.env.GUILD_ID
-)
-),
-{
-body: commands
+    console.log('Commands registered.');
+  } catch (error) {
+    console.error(error);
+  }
 }
-);
-
-console.log('Commands registered.');
-
-} catch (error) {
-
-console.error(error);
-
-}
-}// =====================
+// =====================
 // READY EVENT
 // =====================
 
