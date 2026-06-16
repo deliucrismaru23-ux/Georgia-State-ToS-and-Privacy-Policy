ea676499402a7d6850e2205da9a3954be3639f95
deliucrismaru23-ux <deliucrismaru23@gmail.com>
2026-06-16 12:18:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -825,7 +825,7 @@ const embed = new EmbedBuilder()
     `Powered by Georgia State Roleplay`
   )
   .setColor('Blue')
-  .setThumbnail('https://cdn.discordapp.com/attachments/1509267092824915988/1516399635692785694/Tickets.png?ex=6a3280b6&is=6a312f36&hm=802d9f2e8d8fdc4a22eefc694ce376c7fa2b4398e49c6de57bbedae137e9a881');
+  .setImage('https://cdn.discordapp.com/attachments/1509267092824915988/1516399635692785694/Tickets.png?ex=6a3280b6&is=6a312f36&hm=802d9f2e8d8fdc4a22eefc694ce376c7fa2b4398e49c6de57bbedae137e9a881');
 
   const menu = new StringSelectMenuBuilder()
     .setCustomId('ticket_select')
